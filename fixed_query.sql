22a0148528c2137894d12465a5b276c090beedaa
Kaustav Mitra <anaybis11@gmail.com>
2025-08-30 17:08:39 +0530

    Remove accidentally committed SQL files

--- fixed_query.sql
+++
@@ -1,8 +0,0 @@
-WITH vul_val_data AS (
-    SELECT customer_number, sales_org, cust_name, COUNT(customer_number) AS count
-    FROM lux_staging.customer_master 
-    GROUP BY customer_number, sales_org, cust_name
-)
-SELECT v.customer_number, v.sales_org, v.cust_name, v.count
-FROM vul_val_data v
-LEFT JOIN lux_staging.sales_master_finalv9 s ON v.customer_number = s."Sold To Party";
